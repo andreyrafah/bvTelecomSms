7b650a1f7a577dcff89ccad8f893db84b0da6852
andreyrafah <andreyrafah@users.noreply.github.com>
2023-10-09 13:06:41 +0000

    Fix styling

--- src/InvalidNumberException.php
+++ src/InvalidNumberException.php
@@ -1,11 +1,13 @@
 <?php
+
 namespace Andreyrafah\BvTelecomSms;
 
 use Throwable;
 
 class InvalidNumberException extends \Exception
 {
-    public function __construct($message, $code = 400, Throwable $previous = null) {
+    public function __construct($message, $code = 400, Throwable $previous = null)
+    {
         parent::__construct($message, $code, $previous);
     }
 }
